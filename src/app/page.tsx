f9f762388c1773a4010ee596288363864dd5e720
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2023-07-09 02:27:34 +0900

    add daisyUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default async function Home() {
   return (
     <main className="">
-      
+      <button className="btn btn-primary">Hello daisyUI</button>
     </main>
   )
 }
