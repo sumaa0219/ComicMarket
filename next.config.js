b3a7b4aea87b98f2cd8040c073d3d6e924d8d126
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2023-07-09 02:08:56 +0900

    created project directory, removed old pages/ file temporary

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-module.exports = {
-  reactStrictMode: false,
-  serverRuntimeConfig: {
-    // SSL証明書と秘密鍵のパスを指定
-    api: {
-      bodyParser: {
-        sizeLimit: '1gb', // ボディサイズ制限を増やす（例: 1GB）
-      },
-    },
-  },
-
-};
-
+module.exports = nextConfig
