496b50a01d3579df132b001f17fb29f59e9df3ff
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2023-07-16 15:09:49 +0900

    メインページをちょっと書いた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import Layout from "@/components/layout";
+import { useAuth } from "@/hooks/auth";
+import { auth } from "@/lib/firebase";
+import Link from "next/link";
 
 export default function Home() {
+  const { user } = useAuth(auth)
   return (
     <Layout>
-      ここ何のページにするか悩む
-      追加は左上のメニューから
+      <div className="flex flex-col">
+        <Link className="text-xl mb-2 link" href="/add">サークル/購入物追加</Link>
+        <Link className="text-xl mb-2 link" href="/item/list">購入物一覧</Link>
+        <Link className="text-xl mb-2 link" href={`/user/${user?.uid}`}>自分の購入物一覧</Link>
+      </div>
     </Layout>
   )
 }
