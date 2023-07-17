7227125afaf3eae1411cfa26791d093441e2a8e4
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2023-07-18 01:09:23 +0900

    disable sentry on development

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -11,9 +11,10 @@ Sentry.init({
 
   integrations: [
     new ProfilingIntegration(),
-  ]
+  ],
 
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
+  enabled: process.env.NODE_ENV !== "development",
 });
