99e12258929ff0fa97f0c8e90568292f428933d3
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2023-08-08 22:58:42 +0900

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app'
 import NextNProgress from 'nextjs-progressbar';
 import { Fragment } from 'react';
 import Maintenance from './maintenance';
+import { Analytics } from '@vercel/analytics/react';
 
 export default process.env.NEXT_PUBLIC_MAINTENANCE_MODE?.toLowerCase() === 'true' ? Maintenance : function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <NextNProgress />
       <Component {...pageProps} />
+      <Analytics />
     </Fragment>
   )
 }
