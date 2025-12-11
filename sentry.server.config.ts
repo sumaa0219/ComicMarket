4a608c072b220fde53668e759fbfa40f00efaf49
sumaa0219 <sumaa0219@gmail.com>
2025-12-11 09:35:20 +0900

    fix: remove @sentry/profiling-node for Vercel compatibility

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -3,20 +3,12 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
-import { ProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
   dsn: "https://cd5274e27d44cd1f302d4175b0cb4c80@o174081.ingest.sentry.io/4506446507999232",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
-  // Profiling sample rate is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-
-  integrations: [
-    // Add profiling integration to list of integrations
-    new ProfilingIntegration(),
-  ],
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
